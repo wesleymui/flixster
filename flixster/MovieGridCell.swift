f3df25f5f01979e92be4c53d834c988892f18cee
wesleymui <68392398+wesleymui@users.noreply.github.com>
2021-02-21 20:26:48 -0500

    Flix Part 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Wesley Mui on 2/21/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+}
